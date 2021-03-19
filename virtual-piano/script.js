3e8be987142f0bec02fee87f0b94e249e179ca97
Elena Krivina <alena1027@gmail.com>
2021-03-19 19:58:13 -0300

    feat:add piano-key-letter class to Letter button if press;add mouseover & mouseout event

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,54 +1,55 @@
 const piano = document.querySelector('.piano');
 const buttonSwitch = document.querySelector('.btn-container')
-const pianoKeys = document.querySelector('.piano-key')
+const pianoKeys = document.querySelectorAll('.piano-key');
 const btnLetters = document.querySelector('#letters');
 const btnNotes = document.querySelector('#notes');
 const btns = document.querySelector('.btn');
 const btnFullScreen = document.querySelector('#fullScreenBtn');
-
+const containerBtn = document.querySelector('.btn-container');
 
 
 //Full screen toggle
-btnFullScreen.addEventListener("click", function(e) {
-    toggleFullScreen();
-}, false);
-
+btnFullScreen.addEventListener("click", toggleFullScreen);
 
 function toggleFullScreen() {
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
     } else {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        }
+        document.exitFullscreen();
     }
 }
 
 //Switch between buttons
-btnNotes.addEventListener('click', function(event) {
+containerBtn.addEventListener('click', (event) => {
     if (!event.target.classList.contains('btn-active')) {
-        btnLetters.classList.remove('btn-active');
-        event.target.classList.add('btn-active');
-
+        btnLetters.classList.toggle('btn-active');
+        btnNotes.classList.toggle('btn-active');
+        toggleLActiveLetterClass();
     }
 });
-
-btnLetters.addEventListener('click', function(event) {
-    if (!event.target.classList.contains('btn-active')) {
-        btnNotes.classList.remove('btn-active');
-        event.target.classList.add('btn-active');
-        pianoKeys.classList.add('piano-key-letter');
-
-    }
-});
-
 
 //События мыши
+pianoKeys.forEach((elem) => {
+    elem.addEventListener('mouseover', (event) => {
+        const key = event.target;
+        if (event.buttons === 1 && key.classList.contains('piano-key')) {
+            toggleActiveClasses(key);
+            const audio = document.querySelector(`audio[data-key = "Key${key.dataset.letter}"]`);
+            playAudio(audio);
+        }
+    })
+    elem.addEventListener('mouseout', (event) => {
+        if (event.target.classList.contains('piano-key-active')) {
+            toggleActiveClasses(event.target);
+        }
+    })
+})
+
 piano.addEventListener('mousedown', (event) => {
     const key = event.target;
     if (key.classList.contains('piano-key')) {
         toggleActiveClasses(key);
-        const audio = document.querySelector(`audio[data-key="Key${event.target.dataset.letter}"`);
+        const audio = document.querySelector(`audio[data-key = "Key${key.dataset.letter}"]`);
         playAudio(audio);
     }
 });
@@ -62,21 +63,26 @@ piano.addEventListener('mouseup', (event) => {
 
 //При нажатии клавиши клавиатуры вызываем функцию playAudio
 window.addEventListener('keydown', (event) => {
-    const key = document.querySelector(`div[data-key="${event.code}"]`);
+    const key = event.target;
     if (!key.classList.contains('piano-key-active')) {
         toggleActiveClasses(key);
-        const audio = document.querySelector(`audio[data-key="${event.code}"]`);
+        const audio = document.querySelector(`audio[data-key = "${event.code}"]`);
         playAudio(audio);
     }
 });
 
 window.addEventListener('keyup', (event) => {
-    const key = document.querySelector(`div[data-key="${event.code}"]`);
+    const key = document.querySelector(`div[data-key = "${event.code}"] `);
     toggleActiveClasses(key);
 });
 
 function toggleActiveClasses(key) {
     ['piano-key-active', 'piano-key-active-pseudo', 'piano-key-remove-mouse'].forEach(el => key.classList.toggle(el));
+}
+
+function toggleLActiveLetterClass() {
+
+    pianoKeys.forEach(el => el.classList.toggle('piano-key-letter'));
 }
 
 
