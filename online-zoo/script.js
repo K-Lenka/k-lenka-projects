a63cec02c7387e345246015de614bead77bd310d
Elena Krivina <alena1027@gmail.com>
2021-04-17 19:37:46 -0300

    feat: make an accordion on Other cams section, add shadow effect on video images

--- online-zoo/script.js
+++ online-zoo/script.js
@@ -1,5 +1,7 @@
 var acc = document.getElementsByClassName("accordion");
+var acc1 = document.getElementsByClassName("accordion1");
 var i;
+var j;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
@@ -9,6 +11,18 @@ for (i = 0; i < acc.length; i++) {
             panel.style.maxHeight = null;
         } else {
             panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+for (j = 0; j < acc1.length; j++) {
+    acc1[j].addEventListener("click", function() {
+        this.classList.toggle("active1");
+        var panel1 = this.nextElementSibling;
+        if (panel1.style.maxHeight) {
+            panel1.style.maxHeight = null;
+        } else {
+            panel1.style.maxHeight = panel1.scrollHeight + "px";
         }
     });
 }
